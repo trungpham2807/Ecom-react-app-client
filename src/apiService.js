107be457a6b76988f08fc126bc1f6fdd6e9a661e
leonero2807 <trungpham280797@gmail.com>
2022-02-20 17:48:20 +0700

    update deploy with BE

--- src/apiService.js
+++ src/apiService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-const BASE_URL = "http://localhost:5000/api";
-
+// const BASE_URL = "http://localhost:5000/api";
+const BASE_URL="https://ecom-react-app-server.herokuapp.com/api"
 const user = JSON.parse(localStorage.getItem("persist:root"))?.user;
 const currentUser = user && JSON.parse(user).currentUser;
 const TOKEN = currentUser?.accessToken;
